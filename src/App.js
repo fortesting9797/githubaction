d45f052fa3a0041f7878a9a50d068b1d851fdd4e
root <root@ip-172-31-18-85.ec2.internal>
2025-02-27 10:06:23 +0000

    updated 25

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO EASYDEPLOY
+          WELCOME TO EASYDEPLOY:Deeepika
         </p>
         <a
           className="App-link"
