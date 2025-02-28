d154a9d4cd56f0c8f83b22baefcfa84061242c5f
fortesting9797 <fortesting9797@gmail.com>
2025-02-28 14:22:08 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO REACT: pro max 1.3
+          WELCOME TO REACT: pro max 1.1
         </p>
         <a
           className="App-link"
